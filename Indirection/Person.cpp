d03da0949b477306e14e30c3fdf8cc3cd43de121
Jack Huang <jackhwl@hotmail.com>
2018-05-18 22:44:07 -0400

    t9 test and Indirection code

---
+++ Indirection/Person.cpp
@@ -0,0 +1,18 @@
+#include "Person.h"
+
+using std::string;
+
+Person::Person(string first, string last, int arbitrary) : firstname(first), lastname(last), arbitrarynumber(arbitrary)
+{
+
+}
+
+Person::~Person()
+{
+
+}
+
+string Person::GetName() const
+{
+	return firstname + " " + lastname + " " + std::to_string(arbitrarynumber);
+}
