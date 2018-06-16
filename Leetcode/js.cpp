0d6b1dd349e27c931a533122af9b8d613113e674
Jack Huang <jackhwl@hotmail.com>
2018-06-16 14:51:22 -0400

    pubsub charlenge

--- Leetcode/js.cpp
+++ Leetcode/js.cpp
@@ -411,22 +411,179 @@ function exp(value) {
 var nae = [Math.sqrt, [add, [square, 3], [square, 4]]];
 console.log(exp(nae));
 */
-/*
-Write a function addg that adds from many invocations, until it sees an empty invocation.
-addg()   //undefined
-addg(2)()  //2
-addg(2)(7)() //9
-*/
+
 function addg(a) {
-	var sum = a;
-	if (a == = undefined) {
-		return sum;
+	//var sum = a;
+	if (a != = undefined) {
+		return function more(b) {
+			if (b == = undefined) {
+				return a;
+			}
+			a += b;
+			return more;
+		};
 	}
-	sum += a;
-	return function(b) {
-		addg(b);
+}
+/*
+console.log(addg(2)(4)());
+console.log(addg(2)(4)(5)(3)());
+*/
+
+function liftg0(binary) {
+	return function(first) {
+		function more(next) {
+			if (next == = undefined) {
+				return first;
+			}
+			first = binary(first, next);
+			return more;
+		}
+		if (first != = undefined) {
+			return more;
+		}
+	}
+}
+
+function liftg(binary) {
+	return function(first) {
+		if (first == = undefined) {
+			return first;
+		}
+		return function more(next) {
+			if (next == = undefined) {
+				return first;
+			}
+			first = binary(first, next);
+			return more;
+		};
 	};
 }
 
-console.log(addg(2)());
+/*
+console.log(liftg(mul)(3)(4)());
+console.log(liftg(mul)(3)(0)(4)());
+console.log(liftg(add)(3)(0)(4)());
+*/
 
+function arrayg0(a) {
+	var arr = [];
+	//if (a===undefined) {
+	//  return arr;
+	//}
+	//arr.push(a);
+	return function more(next) {
+		if (next == = undefined) {
+			return arr;
+		}
+		arr.push(next);
+		return more;
+	}(a)
+}
+
+function arrayg1(a) {
+	var arr = [];
+	function more(next) {
+		if (next == = undefined) {
+			return arr;
+		}
+		arr.push(next);
+		return more;
+	}
+	return more(a);
+}
+
+function arrayg(first) {
+	if (first == = undefined) {
+		return[];
+	}
+	return liftg(
+		function(array, value) {
+		array.push(value);
+		return array;
+	}
+	)([first]);
+}
+/*
+console.log(arrayg());
+console.log(arrayg(3)());
+console.log(arrayg(3)(4)(5)());
+*/
+
+function continuize(unary) {
+	return function(callback, arg) {
+		return callback(unary(arg));
+	};
+}
+
+/*
+var sqrtc = continuize(Math.sqrt);
+sqrtc(console.log, 81);
+*/
+
+function vector() {
+	var array = [];
+
+	return {
+	get: function(index) {
+		return array[index];
+	},
+		store : function(index, m) {
+		array[index] = m;
+	},
+		append : function(n) {
+		array.push(n);
+	}
+	};
+}
+/*
+var vec =  vector();
+var stash;
+//var array = [1];
+var tt;
+vec.append(3);
+vec.append(4);
+
+vec.store('push', function() { stash = this;});
+//vec.get2 = function(){return this.array;};
+console.log(vec.get(0));
+console.log(vec.get(1));
+console.log(vec.append());
+console.log(stash);
+//console.log(vec.get('this.array'));
+*/
+
+function pubsub() {
+	var subscribers = [];
+	return Object.freeze({
+	subscribe: function(subscriber) {
+		subscribers.push(subscriber);
+	},
+		publish : function(publication) {
+		for (var i = 0; i<subscribers.length; i += 1) {
+			try {
+				subscribers[i](publication);
+			}
+			catch (ignore) {}
+		}
+	}
+		});
+}
+
+var my_pubsub = pubsub();
+my_pubsub.subscribe(console.log);
+my_pubsub.publish('hello');
+my_pubsub.subscribe(function() { console.log(this); this.length = 0; });
+my_pubsub.publish('hello2');
+my_pubsub.publish('hello3');
+my_pubsub.publish('hello4');
+
+/*
+var ab = [];
+ab.push(1);
+ab.push(2);
+console.log(ab);
+ab.push(undefined);
+ab.push(3);
+ab.push(4);
+console.log(ab);
+*/
