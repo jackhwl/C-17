72bdad474ff72a7987752d58d26717e3c6f84a7a
Jack Huang <jackhwl@hotmail.com>
2018-06-21 21:54:09 -0400

    002.js

---
+++ Leetcode/002.js
@@ -0,0 +1,34 @@
+// 96.15%
+var addTwoNumbers = function (l1, l2) {
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+    let mod = (l1.val + l2.val) % 10;
+    let extra = (l1.val + l2.val) > 9 ? 1 : 0;
+
+    let lsum = new ListNode(mod);
+    if (l1.next == null) {
+        lsum.next = extra > 0 ? addTwoNumbers(new ListNode(extra), l2.next) : l2.next;
+        return lsum;
+    }
+    if (l2.next == null) {
+        lsum.next = extra > 0 ? addTwoNumbers(new ListNode(extra), l1.next) : l1.next;
+        return lsum;
+    }
+    lsum.val = mod;
+    let l2withextra = extra > 0 ? addTwoNumbers(new ListNode(extra), l2.next) : l2.next;
+    lsum.next = addTwoNumbers(l1.next, l2withextra);
+    return lsum;
+};
+
+var addTwoNumbers = function (l1, l2) {
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+    let mod = (l1.val + l2.val) % 10;
+    let extra = (l1.val + l2.val) > 9 ? 1 : 0;
+
+    let lsum = new ListNode(mod);
+    lsum.val = mod;
+    let l1AddExtra = extra > 0 ? addTwoNumbers(new ListNode(extra), l1.next) : l1.next;
+    lsum.next = addTwoNumbers(l1AddExtra, l2.next);
+    return lsum;
+};
